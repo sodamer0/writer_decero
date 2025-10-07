8163eba2e44305c48c5cbd5ec8e07ff5007103fd
sergio <sergio>
2025-10-07 08:11:35 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+
+    public static void welcome(){
+        System.out.println();
+        System.out.println("############ Writer ############");
+        System.out.println("################################");
+        System.out.println("1 . Escribir en un archivo");
+        System.out.println("2 . Leer un archivo");
+        System.out.println("3 . Borrar un archivo");
+        System.out.println("4 . Listar ficheros de un directorio");
+        System.out.println("0 . Salir");
+        System.out.println("################################");
+    }
+}
