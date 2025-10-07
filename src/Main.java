aff6ca9103cdc7bddccc9a8bf80dd552c8baa2dc
Santiago Pérez Carral <a25santiagopc@teis.intranet>
2025-10-07 17:44:38 +0200

    Empezando writer project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,47 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileReader;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+  private static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        welcome();
+
+      welcome();
+
+      int opcion = teclado.nextInt();
+      teclado.nextLine();
+
+      switch (opcion){
+        case 1:
+            nombrarFile();
+            teclado.close();
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          break;
+        case 0:
+          break;
+        default:
+          System.out.println("Por favor, introduzca una opción válida.");
+          break;
+      }
+
+
+
+
+
     }
+
 
 
     public static void welcome(){
@@ -18,4 +56,36 @@ public class Main {
         System.out.println("0 . Salir");
         System.out.println("################################");
     }
+
+
+    public static void nombrarFile(){
+      System.out.println("Introduzca el nombre del archivo ->");
+      String fileName = teclado.nextLine();
+      System.out.println("Texto:");
+      String contenido = teclado.nextLine();
+      try(FileWriter escritura = new FileWriter(fileName)){
+        escritura.write(contenido);
+        System.out.println("El archivo " + fileName + " ha sido creado/modificado con éxito.");
+      } catch (IOException e) {
+        System.err.println("Error al crear/modificar el archivo." + e.getMessage());
+      }
+    }
+
+    public static void leerFile(){
+
+      System.out.println("Introduzca el nombre del archivo que desea leer ->");
+      String fileName = teclado.nextLine();
+      try(FileReader lector = new FileReader(fileName)){
+
+      } catch (FileNotFoundException e) {
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+
+
+
+
 }
